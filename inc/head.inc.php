f0c2f3e27daf7230773357505b621ebe21935641
Dark4MD <github.web@manu.li>
2021-06-06 22:07:16 +0200

    New Cookie Consent with Impressum

--- inc/head.inc.php
+++ inc/head.inc.php
@@ -33,12 +33,16 @@
     <link href="css/demo.css" rel="stylesheet"><!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
-    <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
-    <script type="text/javascript">
-        window.cookieconsent_options = {"message":"Diese Website nutzt Cookies um, das Surfverhalten zu verbessern.","dismiss":"Verstanden!","learnMore":"Mehr Infos!","link":"https://ff-steinhude.de/impressum.php","theme":"dark-top"};
-    </script>
+<script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="1bc4de8b-ea4e-4b92-8568-8f7a49e5366d" data-blockingmode="auto" type="text/javascript"></script>
+<script data-ad-client="ca-pub-7845897898534700" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129873055-5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js"></script>
-    <!-- End Cookie Consent plugin -->
+  gtag('config', 'UA-129873055-5');
+</script>
 
 </head>
